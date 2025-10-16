2050ba98dcd2f02431fdccacd9a51ffe1f5ae88b
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2025-10-16 22:38:17 +0800

    Change email transport security settings

--- BACKEND/src/api/helpers/sendOtpEmail.js
+++ BACKEND/src/api/helpers/sendOtpEmail.js
@@ -7,7 +7,8 @@ export async function sendOtpEmail(toEmail, otp) {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
-      secure: true,
+      secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD,
